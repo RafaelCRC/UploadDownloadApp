d101888647f48882cecbe8b9ce026126d1a64b11
RafaelCRC <rafael.cardoso@liferay.com>
2023-07-26 21:20:20 -0300

    CORS and body-parser

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const uploadRoute = require('./routes/upload');
 const downloadRoute = require('./routes/download');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Acess-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
 
 app.use('/upload', uploadRoute);
 app.use('/download', downloadRoute);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found.')
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
