0b0194fe4afe1bc446af6f83843598e7f0af31d0
RafaelCRC <rafael.cardoso@liferay.com>
2023-07-26 21:13:02 -0300

    add morgan for dev test

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const uploadRoute = require('./routes/upload');
 const downloadRoute = require('./routes/download');
+
+app.use(morgan('dev'));
 
 app.use('/upload', uploadRoute);
 app.use('/download', downloadRoute);
